f3f0ff6778b8f48c9d0720ec2fd7d7581f48ecce
GSzymon <szymek6786@gmail.com>
2017-07-30 20:32:00 +0200

    Add files via upload

---
+++ Models/FieldState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Battleship_asp.Models
+{
+    public enum EnumFieldState { Empty = 1, OccupiedAlive = 2, OccupiedDead = 3, Missed = 4 };
+
+    public class FieldState
+    {
+        public EnumFieldState state { get; private set; }
+
+
+        public FieldState()                             
+        {
+            state = EnumFieldState.Empty;               // EMPTY <- default  (ctor)
+        }
+
+        public void SetShipHere()
+        {
+            state = EnumFieldState.OccupiedAlive;       // OCCUPIED ALIVE
+        }
+
+        public bool ShowResultAndUpdateFieldState()
+        {
+            if (state == EnumFieldState.Empty)
+            {
+                state = EnumFieldState.Missed;
+                return false;
+            }
+            else if (state == EnumFieldState.OccupiedAlive)
+            {
+                state = EnumFieldState.OccupiedDead;
+                return false;
+            }
+            else
+            {
+                throw new Exception("Error during checking shoot -> class: FieldState, method: UpdateField");
+            }
+        }
+    }
+}
---
+++ MyLibrary/MyMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Battleship_asp.MyLibrary
+{
+    public static class MyMethods
+    {
+        public static char IntToChar(int x)
+        {
+            if (x==0) { return 'A';}
+            if (x == 1) { return 'B'; }
+            if (x == 2) { return 'C'; }
+            if (x == 3) { return 'D'; }
+            if (x == 4) { return 'E'; }
+            if (x == 5) { return 'F'; }
+            if (x == 6) { return 'G'; }
+            if (x == 7) { return 'H'; }
+            if (x == 8) { return 'I'; }
+            if (x == 9) { return 'J'; }
+            
+            else { return 'X'; }
+        }
+    }
+}
